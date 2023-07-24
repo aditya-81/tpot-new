de252042b41e9d91d0954da726f1d26489c4c615
Aditya Bhat <bhataditya.off@gmail.com>
2023-07-24 16:01:05 +0530

    changed git link in code

--- iso/installer/install.sh
+++ iso/installer/install.sh
@@ -709,7 +709,7 @@ if ! [ "$myTPOT_DEPLOYMENT_TYPE" == "iso" ];
   then
     fuBANNER "Cloning T-Pot"
     ### DEV
-    git clone https://github.com/telekom-security/tpotce /opt/tpot
+    git clone https://github.com/aditya-81/tpot-new.git /opt/tpot
 fi
 
 # Let's create the T-Pot user
